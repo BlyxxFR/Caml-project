3daec0b0cf1e8d908b64e5836b94d09a6180429d
BlyxxFR <blyxx70@gmail.com>
2017-11-15 12:13:01 +0100

    Added todo

--- src/algo/fordFulkerson.ml
+++ src/algo/fordFulkerson.ml
@@ -12,6 +12,7 @@ let init_algo graph =
                      vi.inedges) ;
     f
 
+    (* TODO : changer le cehmin par la lsite des arrêtes *)
 let chemin graph src dest =
   let settled = ref [dest] in
   let _chemin = ref [dest] in
@@ -21,15 +22,15 @@ let chemin graph src dest =
         | (_, id)::r when List.exists (fun x -> x = Graph.find_vertex graph id) !settled -> trouver_sommet r
         | (label, _)::r when label = "0" -> trouver_sommet r
         | (_, id)::_-> let sommet = Graph.find_vertex graph id in settled := sommet::!settled ; sommet  
-        | _ -> failwith "Pas de chemin"
+        | _ -> failwith "Pas de sommet"
     in
     let sommet_courant = trouver_sommet dest.inedges in
       if sommet_courant <> src 
       then begin _chemin := sommet_courant::!_chemin ; trouver_chemin src sommet_courant end
-      else begin _chemin := src::!_chemin ; !_chemin end
+      else begin _chemin end
 
   in
-    trouver_chemin src dest
+    trouver_chemin src dest ; _chemin := src::!_chemin ; !_chemin
 
 
 
