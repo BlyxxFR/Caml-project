ff87657432b2e937db25202efe8a0451ee8dd720
BlyxxFR <blyxx70@gmail.com>
2017-11-15 09:36:33 +0100

    fix

--- src/make.sh
+++ src/make.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# rm -f *.cmi *.cmo ftest
 ocamlbuild -Is lib,algo ftest.native
-
-# Si ça marche pas, peut être un truc comme ça
-# ocamlc -I lib/ -c lib/gfile.ml
-# ocamlc -I lib/ -c Bureau/graph.ml
-# ocamlc -o ftest lib/gfile.cmo lib/graph.cmo ftest.ml
-
-'
